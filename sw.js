18daea62fa004cf2bfaaf6bfffce11f949a3f6da
Joabcode <a32174@aecister.pt>
2026-05-08 12:19:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-
+// Este evento corre quando a App é "instalada" no telemóvel
+self.addEventListener('install', (e) => {
+ console.log('Service Worker: Instalado');
+});
+// Este evento permite que a App funcione mesmo com picos de falta de rede
+self.addEventListener('fetch', (e) => {
+“My Creative Hub”
+Página 5 de 6
+ // Para este projeto base, apenas deixamos o tráfego passar
+ e.respondWith(fetch(e.request));
+});
